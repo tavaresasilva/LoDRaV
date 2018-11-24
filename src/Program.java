65a5311a049ca4806d10bfca07372caec9105620
alexandre <tavares.alexandresilva@gmail.com>
2018-11-24 20:22:32 -0300

    Adding some comments

--- src/Program.java
+++ src/Program.java
@@ -3,14 +3,15 @@ import contract.IFakeObject;
 
 public class Program {
 
-    public static void main(String args[]){
-
-        var Output = System.out;
+    public static void main(String[] args){
 
         IPerson person = IFakeObject.createAPerson();
 
         //Exemple 1: Violate the Demeter Law
         System.out.println(person.getHouse().getAddress().getZipCode());
+
+        //Prevents the Train Wreck
+        var Output = System.out;
 
         //Exmple 2: Not Violate the Demeter Law
         Output.println(person.getZipCode());
